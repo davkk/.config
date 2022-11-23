8dbe5cac88a4d7c2c6b5ec218cd613439a30838d
Dawid Karp <dadulerk@gmail.com>
2022-11-23 20:36:29 +0100

    add nvim dir

---
+++ nvim/lua/davkk/packer.lua
@@ -0,0 +1,89 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+
+    use 'ellisonleao/gruvbox.nvim'
+
+    use 'nvim-tree/nvim-web-devicons'
+
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable "make" == 1 }
+    use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
+    use 'nvim-telescope/telescope-file-browser.nvim'
+
+    use 'lukas-reineke/indent-blankline.nvim'
+    use {
+        'windwp/nvim-autopairs',
+        config = function() require('nvim-autopairs').setup {} end
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
+    -- use 'nvim-treesitter/nvim-treesitter-context'
+
+    use 'numToStr/Navigator.nvim'
+
+    use {
+        'kylechui/nvim-surround', 
+        tag = '*', -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require('nvim-surround').setup()
+        end
+    }
+
+    use {
+        'abecodes/tabout.nvim',
+        config = function()
+            require('tabout').setup {}
+        end,
+        wants = {'nvim-treesitter'}, -- or require if not used so far
+        after = {'nvim-cmp'} -- if a completion plugin is using tabs load it before
+    }
+
+    use { 
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+
+    use 'nvim-lualine/lualine.nvim'
+
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+        end
+    }
+
+    use 'gpanders/editorconfig.nvim'
+
+    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+    use 'onsails/lspkind-nvim' -- vscode-like pictograms
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+
+    use 'ionide/Ionide-vim'
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
---
+++ nvim/test.fsx
@@ -0,0 +1,8 @@
+let square x =
+    let something = "nothig"
+    float (x * x)
+
+let add222 = (+) 2.
+
+14 |> square
+
