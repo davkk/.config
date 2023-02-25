48e1e8a8a0414627d6c492b32d6e957747855b50
Dawid Karp <dadulerk@gmail.com>
2023-02-25 13:33:04 +0100

    rename file, enable vim-fugitive

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,5 +1,9 @@
 return {
-    "tpope/vim-fugitive",
+    {
+        "tpope/vim-fugitive",
+        event = "BufReadPost",
+        config = true,
+    },
     {
         "lewis6991/gitsigns.nvim",
         event = "BufReadPre",
