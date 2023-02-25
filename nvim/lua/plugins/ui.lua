d516e405cf326f24186196a2ac9965d9e43b8f1a
Dawid Karp <dadulerk@gmail.com>
2023-02-25 20:37:09 +0100

    style lualine and add rounded corners to lualine components

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -9,7 +9,7 @@ return {
             context_char = '▏',
             show_current_context = true,
         },
-        config = function (_, opts)
+        config = function(_, opts)
             require("indent_blankline").setup(opts)
 
             vim.cmd [[highlight IndentBlanklineChar guifg=#1f1d2e gui=nocombine]]
@@ -31,58 +31,77 @@ return {
             custom_rose_pine.insert.c.bg = "None"
             custom_rose_pine.visual.c.bg = "None"
             custom_rose_pine.command.c.bg = "None"
+            custom_rose_pine.replace.c.bg = "None"
 
             return {
                 options = {
                     icons_enabled = true,
                     theme = custom_rose_pine,
-                    component_separators = '',
-                    section_separators = '',
+                    section_separators = "",
+                    component_separators = "",
                     disabled_filetypes = {
                         statusline = { "dashboard", "NvimTree", "Outline" },
                     },
                 },
                 sections = {
-                    lualine_a = { 'mode' },
+                    lualine_a = {
+                        {
+                            "mode",
+                            separator = { left = "", right = "", },
+                            padding = 1,
+                        }
+                    },
                     lualine_b = {
                         {
                             "branch",
                             icons_enabled = true,
                             icon = "",
+                            color = { bg = "none" },
+                            padding = { left = 2, right = 1 },
                         },
+                    },
+                    lualine_c = {
                         {
                             "diff",
                             colored = true,
+                            separator = { left = "", right = "", },
                             diff_color = {
                                 added = { fg = "#6e6a86", bg = "None" },
                                 modified = { fg = "#6e6a86", bg = "None" },
-                                removed  = { fg = "#6e6a86", bg = "None" },
+                                removed = { fg = "#6e6a86", bg = "None" },
                             },
-                            -- symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+                            symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
                             cond = hide_in_width,
                         },
                     },
-                    lualine_c = {},
-                    lualine_x = { "%=" },
-                    lualine_y = {
+                    lualine_x = {
                         {
                             "diagnostics",
                             sources = { "nvim_diagnostic" },
-                            sections = { "error", "warn" },
+                            -- sections = { "error", "warn" },
                             symbols = { error = " ", warn = " ", hint = " ", info = " " },
-                            colored = false,
-                            update_in_insert = false,
-                            always_visible = true,
-                        }
+                            colored = true,
+                            -- diagnostics_color = {
+                            --     error = { fg = "#6e6a86", bg = "None" },
+                            --     warn = { fg = "#6e6a86", bg = "None" },
+                            --     info = { fg = "#6e6a86", bg = "None" },
+                            --     hint = { fg = "#6e6a86", bg = "None" },
+                            -- },
+                            update_in_insert = true,
+                            -- always_visible = true,
+                        },
                     },
+                    lualine_y = {},
                     lualine_z = {
-                        {
-                            function()
-                                local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-                                if buf_ft == "toggleterm" then return "" end
-                                return buf_ft
-                            end,
-                        }
+                        -- {
+                        --     function()
+                        --         local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+                        --         if buf_ft == "toggleterm" then return "" end
+                        --         return buf_ft
+                        --     end,
+                        --     separator = { left = "", right = "", },
+                        --     padding = 1,
+                        -- },
                     },
                 },
             }
