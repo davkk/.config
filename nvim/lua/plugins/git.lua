852139082bafc0ed52960163199304eacded9ff4
Dawid Karp <dadulerk@gmail.com>
2023-02-25 13:53:22 +0100

    configure vim fugitive keymaps, restore previous file name

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "tpope/vim-fugitive",
+        cmd = { "G", "Git" },
+        keys = { "<leader>g", },
+        config = function()
+            local keymap = vim.keymap
+            local opts = { noremap = true, silent = true }
+
+            keymap.set('n', '<leader>gg', '<cmd>:G<CR>', opts)
+            keymap.set('n', '<leader>gw', '<cmd>:Gwrite<CR>', opts)
+            keymap.set('n', '<leader>gp', '<cmd>:G push<CR>', opts)
+        end
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "BufReadPre",
+        config = true,
+    },
+}
