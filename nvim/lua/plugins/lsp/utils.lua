b6fc2b1603a206837d3bf8c540221a514face95a
Dawid Karp <dadulerk@gmail.com>
2023-04-17 22:38:28 +0200

    change next and prev error keymaps

--- nvim/lua/plugins/lsp/utils.lua
+++ nvim/lua/plugins/lsp/utils.lua
@@ -55,8 +55,8 @@ M.on_attach = function(_, bufnr)
     -- open detailed error message window
     vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, opts)
 
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]e", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "[e", vim.diagnostic.goto_prev, opts)
 
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
