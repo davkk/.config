0f3040f8a62a54233cb51400c9cc5e6a2a6cb728
Dawid Karp <dadulerk@gmail.com>
2023-04-21 16:35:25 +0200

    disable plugins update notification on startup

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,14 +1,14 @@
 -- ensure lazy is installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,7 +18,7 @@ require("lazy").setup("plugins", {
     ui = {
         border = "rounded",
     },
-    checker = { enabled = true },
+    checker = { enabled = false, notify = false },
     debug = false,
 }
 )
