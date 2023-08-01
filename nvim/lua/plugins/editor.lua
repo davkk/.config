02f9e6c1993a21c793e3bdd1a44f5bb58e9f711d
Dawid Karp <dadulerk@gmail.com>
2023-08-01 22:05:57 +0200

    feat: configure comment.nvim with nvim-ts-context-commentstring

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -60,7 +60,12 @@ return {
     {
         "numToStr/Comment.nvim",
         event = "VeryLazy",
-        config = true,
+        dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+        config = function()
+            require("Comment").setup({
+                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+            })
+        end
     },
     {
         "EtiamNullam/deferred-clipboard.nvim",
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -45,7 +45,7 @@ return {
                 additional_vim_regex_highlighting = false,
             },
             indent = { enable = true },
-            context_commentstring = { enable = true }
+            context_commentstring = { enable = true, enable_autocmd = false }
         },
         config = function(_, opts)
             local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
