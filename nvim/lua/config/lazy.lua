733ddaf31bf7de85258a7b6e8a768b796cb1256a
Dawid Karp <dadulerk@gmail.com>
2023-08-19 09:18:07 +0200

    fix: install missing plugins only when I say so

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-    install = { colorscheme = { "rose-pine" } },
+    install = { colorscheme = { "rose-pine" }, missing = false },
     defaults = { lazy = true },
     ui = {
         border = "rounded",
