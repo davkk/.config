7a58afa77995368ff7008f93675f3a3c6d963348
davkk <dadulerk@gmail.com>
2023-09-17 18:30:33 +0200

    feat: add formatoptions, joinspaces, diffopt

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -105,3 +105,18 @@ opt.listchars = {
     conceal = '┊',
     nbsp = '␣',
 }
+
+opt.formatoptions = opt.formatoptions
+    - "a" -- Auto formatting is BAD.
+    - "t" -- Don't auto format my code. I got linters for that.
+    + "c" -- In general, I like it when comments respect textwidth
+    + "q" -- Allow formatting comments w/ gq
+    - "o" -- O and o, don't continue comments
+    + "r" -- But do continue when pressing enter.
+    + "n" -- Indent past the formatlistpat, not underneath it.
+    + "j" -- Auto-remove comments if possible.
+    - "2" -- I'm not in gradeschool anymore
+
+opt.joinspaces = false -- Two spaces and grade school, we're done
+
+opt.diffopt = { "internal", "filler", "closeoff", "hiddenoff", "algorithm:minimal" }
