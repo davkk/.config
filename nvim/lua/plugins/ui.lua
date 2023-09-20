ea05040c12d19d6221f907c9defa8d6497bdfe59
Dawid Karp <dadulerk@gmail.com>
2023-09-20 23:00:20 +0200

    feat: use tabline from lualine, which replaces winbar; move filename/path to bottom right

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -19,9 +19,20 @@ return {
             custom_rose_pine.command.c.bg = "None"
             custom_rose_pine.replace.c.bg = "None"
 
-            local my_filename = require('lualine.components.filename'):extend()
-            my_filename.apply_icon = require('lualine.components.filetype').apply_icon
-            my_filename.icon_hl_cache = {}
+            local extend_filename = require('lualine.components.filename'):extend()
+            extend_filename.apply_icon = require('lualine.components.filetype').apply_icon
+            extend_filename.icon_hl_cache = {}
+
+            local custom_filename = {
+                extend_filename,
+                color = { gui = "bold" },
+
+                file_status = true,
+                newfile_status = false,
+                path = 1,
+
+                shorting_target = 30,
+            }
 
             return {
                 options = {
@@ -29,30 +40,36 @@ return {
                     theme = custom_rose_pine,
                     section_separators = "",
                     component_separators = "",
+                    always_divide_middle = false,
+                    globalstatus = true,
                 },
-                winbar = {
+                tabline = {
                     lualine_a = {},
                     lualine_b = {},
                     lualine_c = {
                         {
-                            my_filename,
-                            color = { gui = "bold" },
-
-                            file_status = true,
-                            newfile_status = false,
-                            path = 1,
-
-                            shorting_target = 40,
-                        }
+                            "tabs",
+                            mode = 1,
+                            tabs_color = {
+                                active = { gui = "bold" },
+                                inactive = { bg = "none", fg = "#908caa", gui = "bold" },
+                            },
+                            fmt = function(name, context)
+                                return string.format("%s:[%s]", context.tabnr, name)
+                            end,
+                        },
                     },
                     lualine_x = {
                         {
                             "location",
-                            color = { fg = "#6e6a86", gui = "bold" },
+                            color = { fg = "#908caa", gui = "bold" },
+                            cond = function()
+                                return vim.fn.winwidth(0) > 80
+                            end
                         },
                     },
                     lualine_y = {},
-                    lualine_z = {},
+                    lualine_z = {}
                 },
                 sections = {
                     lualine_a = {},
@@ -62,6 +79,14 @@ return {
                             icon = "",
                             color = { bg = "none", gui = "bold" },
                             padding = { right = 1 },
+                            fmt = function(str)
+                                local limit = 25
+                                if #str > limit then
+                                    return str:sub(1, limit) .. "…"
+                                else
+                                    return str
+                                end
+                            end,
                         },
                         {
                             "diff",
@@ -76,13 +101,14 @@ return {
                                 modified = " ",
                                 removed = " "
                             },
-                            cond = function ()
+                            cond = function()
                                 return vim.fn.winwidth(0) > 80
                             end
                         },
                     },
                     lualine_c = {},
                     lualine_x = {
+                        custom_filename,
                         {
                             "diagnostics",
                             sources = { "nvim_diagnostic" },
