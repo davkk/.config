82f2b1866b70a82f67fafe1ba4bf6b594e2ea4ac
Dawid Karp <dadulerk@gmail.com>
2023-09-22 00:12:07 +0200

    feat: enable tabbar in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,10 +14,10 @@ config.adjust_window_size_when_changing_font_size = false
 config.hide_mouse_cursor_when_typing = false
 
 -- TABS
-config.enable_tab_bar = false
 config.use_fancy_tab_bar = false
-config.show_tabs_in_tab_bar = false
+config.tab_bar_at_bottom = true
 config.show_new_tab_button_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 
 -- WINDOW
 config.initial_cols = 120
@@ -28,10 +28,10 @@ config.cell_width = 0.9
 config.window_background_opacity = 0.8
 config.window_decorations = "RESIZE"
 config.window_padding = {
-    left = "2cell",
-    right = "2cell",
-    top = "0.9cell",
-    bottom = "0.9cell",
+    left = "1cell",
+    right = "1cell",
+    top = "0.5cell",
+    bottom = "0.5cell",
 }
 
 -- COLORS
@@ -82,9 +82,34 @@ config.colors = {
     },
 
     tab_bar = {
-        background = palette.base,
-        inactive_tab_edge = palette.muted,
-    },
+        background = "none",
+
+        active_tab = {
+            bg_color = "none",
+            fg_color = palette.text,
+            intensity = "Bold",
+        },
+
+        inactive_tab = {
+            bg_color = "none",
+            fg_color = palette.muted,
+        },
+
+        inactive_tab_hover = {
+            bg_color = "none",
+            fg_color = palette.muted,
+        },
+
+        new_tab = {
+            bg_color = "none",
+            fg_color = palette.overlay,
+        },
+
+        new_tab_hover = {
+            bg_color = "none",
+            fg_color = palette.overlay,
+        },
+    }
 }
 
 -- FONT
@@ -123,7 +148,7 @@ config.keys = {
     },
 }
 config.mouse_bindings = {
-        -- Change the default click behavior so that it only selects
+    -- Change the default click behavior so that it only selects
     -- text and doesn"t open hyperlinks
     {
         event = { Up = { streak = 1, button = "Left" } },
