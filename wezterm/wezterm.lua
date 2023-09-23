e3bde8e624f5e7e916dfcce581d7c0e8a3ddbbb2
davkk <dadulerk@gmail.com>
2023-09-23 19:58:14 +0200

    feat: single to double quote strings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -36,18 +36,18 @@ config.window_padding = {
 
 -- COLORS
 local palette = {
-    base = '#1a1821',
-    overlay = '#2F2B3B',
-    muted = '#645C70',
-    subtle = '#8D849A',
-    text = '#dad6e9',
-    love = '#f56389',
-    gold = '#ffb083',
-    rose = '#edb2b5',
-    pine = '#628079',
-    foam = '#b7d7d5',
-    iris = '#d2b1d6',
-    highlight_high = '#545161',
+    base = "#1a1821",
+    overlay = "#2F2B3B",
+    muted = "#645C70",
+    subtle = "#8D849A",
+    text = "#dad6e9",
+    love = "#f56389",
+    gold = "#ffb083",
+    rose = "#edb2b5",
+    pine = "#628079",
+    foam = "#b7d7d5",
+    iris = "#d2b1d6",
+    highlight_high = "#545161",
 }
 
 config.colors = {
