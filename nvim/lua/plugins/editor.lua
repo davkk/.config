5e5f059ab62ebf9687718efc66723ae214b3ea1c
Dawid Karp <dadulerk@gmail.com>
2023-09-26 22:47:43 +0200

    feat: add vim-surround

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -131,5 +131,9 @@ return {
                 pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
             })
         end
+    },
+    {
+        "tpope/vim-surround",
+        event = "BufReadPost",
     }
 }
