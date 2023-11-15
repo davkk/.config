0f619ab738b6856ddc28f9f119eff17d3bfafb47
davkk <dadulerk@gmail.com>
2023-11-15 22:04:52 +0100

    feat: add keymap for sending visual selection to repl

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -144,8 +144,22 @@ return {
             }
         end,
         keys = {
-            { "<leader>rt", "<cmd>ReplToggle<cr>",  desc = "Toggle nvim-repl" },
-            { "<leader>rc", "<cmd>ReplRunCell<cr>", desc = "nvim-repl run cell" },
+            {
+                "<leader>rt",
+                "<cmd>ReplToggle<cr>",
+                desc = "Toggle nvim-repl"
+            },
+            {
+                "<leader><cr>",
+                "<cmd>ReplRunCell<cr>",
+                desc = "nvim-repl run cell"
+            },
+            {
+                "<leader><cr>",
+                "<Plug>ReplSendVisual<cr>",
+                desc = "nvim-repl send visual",
+                mode = "v",
+            },
         },
     },
     {
