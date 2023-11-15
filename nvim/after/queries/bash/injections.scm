1fa928f73471ea7f2faabd2683c00248d8405d06
Dawid Karp <dadulerk@gmail.com>
2023-11-15 19:31:51 +0100

    feat: update treesitter injection syntax

--- nvim/after/queries/bash/injections.scm
+++ nvim/after/queries/bash/injections.scm
@@ -1,7 +1,25 @@
 ;; extends
+
 (command
-  name: (command_name) @_name
-    (#eq? @_name "awk")
-  argument: ((raw_string) @awk)
-    (#offset! @awk 0 1 0 -1)
-)
+  name: (command_name) @_name (#eq? @_name "awk")
+  argument: (raw_string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "awk"))
+
+(command
+  name: (command_name) @_name (#eq? @_name "awk")
+  argument: (string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "awk"))
+
+(command
+  (word) @_name (#eq? @_name "awk")
+  (raw_string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "awk"))
+
+(command
+  (word) @_name (#eq? @_name "awk")
+  (string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "awk"))
