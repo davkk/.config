fec2db1415835ef53990d6ec7e8658b68ba08c21
davkk <dadulerk@gmail.com>
2023-12-28 17:57:36 +0100

    feat: add codeium

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,5 +1,20 @@
 return {
     {
+        "Exafunction/codeium.vim",
+        event = "BufEnter",
+        config = function()
+            vim.keymap.set('i', '<tab>', function()
+                return vim.fn['codeium#Accept']()
+            end, { expr = true, silent = true })
+            vim.keymap.set('i', '<c-x>', function()
+                return vim.fn['codeium#Clear']()
+            end, { expr = true, silent = true })
+            vim.cmd [[
+                let g:codeium_idle_delay = 1000
+            ]]
+        end
+    },
+    {
         "stevearc/oil.nvim",
         lazy = false,
         dependencies = { "nvim-tree/nvim-web-devicons" },
