707578a65bb43807f221714c095d6ffd9b706594
davkk <dadulerk@gmail.com>
2024-01-05 00:12:51 +0100

    fix: create autocmd for setting the formatoptions

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -110,16 +110,21 @@ opt.listchars = {
     nbsp = '␣',
 }
 
-opt.formatoptions = opt.formatoptions
-    - "a"              -- Auto formatting is BAD.
-    - "t"              -- Don't auto format my code. I got linters for that.
-    + "c"              -- In general, I like it when comments respect textwidth
-    + "q"              -- Allow formatting comments w/ gq
-    - "o"              -- O and o, don't continue comments
-    + "r"              -- But do continue when pressing enter.
-    + "n"              -- Indent past the formatlistpat, not underneath it.
-    + "j"              -- Auto-remove comments if possible.
-    - "2"              -- I'm not in gradeschool anymore
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("FormatOptionsGroup", {}),
+    callback = function()
+        opt.formatoptions = opt.formatoptions
+            - "a" -- Auto formatting is BAD.
+            - "t" -- Don't auto format my code. I got linters for that.
+            + "c" -- In general, I like it when comments respect textwidth
+            + "q" -- Allow formatting comments w/ gq
+            - "o" -- O and o, don't continue comments
+            + "r" -- But do continue when pressing enter.
+            + "n" -- Indent past the formatlistpat, not underneath it.
+            + "j" -- Auto-remove comments if possible.
+            - "2" -- I'm not in gradeschool anymore
+    end
+})
 
 opt.joinspaces = false -- Two spaces and grade school, we're done
 
