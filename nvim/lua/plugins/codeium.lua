7dd6364bcceee22f6537465164b3aff19038cee0
davkk <dadulerk@gmail.com>
2024-01-05 18:11:37 +0100

    fix: reduce the suggestion delay

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -2,16 +2,16 @@ return {
     "Exafunction/codeium.vim",
     event = "BufEnter",
     config = function()
-        vim.keymap.set('i', '<tab>', function()
-            return vim.fn['codeium#Accept']()
+        vim.keymap.set("i", "<tab>", function()
+            return vim.fn["codeium#Accept"]()
         end, { expr = true, silent = true })
 
-        vim.keymap.set('i', '<c-x>', function()
-            return vim.fn['codeium#Clear']()
+        vim.keymap.set("i", "<c-x>", function()
+            return vim.fn["codeium#Clear"]()
         end, { expr = true, silent = true })
 
         vim.cmd [[
-            let g:codeium_idle_delay = 1000
+            let g:codeium_idle_delay = 600
         ]]
     end
 }
