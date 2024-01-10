e93b57ce05e8e40f72e4f5ec840066ba67753b84
davkk <dadulerk@gmail.com>
2024-01-10 23:53:36 +0100

    feat: install fidget from the latest branch

--- nvim/lua/plugins/lsp/fidget.lua
+++ nvim/lua/plugins/lsp/fidget.lua
@@ -1,21 +1,19 @@
 return {
     "j-hui/fidget.nvim",
     event = { "BufReadPost", "BufNewFile" },
-    branch = "legacy",
     opts = {
-        text = {
-            spinner = "line",
-            done = "",
+        progress = {
+            display = {
+                done_ttl = 1,
+                done_icon = "",
+                progress_style = "ModeMsg",
+            },
         },
-        timer = {
-            spinner_rate = 200,      -- frame rate of spinner animation, in ms
-            fidget_decay = 1000,     -- how long to keep around empty fidget, in ms
-            task_decay = 700,        -- how long to keep around completed task, in ms
-        },
-        window = {
-            relative = "editor",
-            blend = 0,
-            zindex = 100,
-        },
+        notification = {
+            window = {
+                normal_hl = "FloatBorder",
+                border = "solid",
+            },
+        }
     },
 }
