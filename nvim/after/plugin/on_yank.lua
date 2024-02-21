90a9ea1b3af7eaa7ed22bb16bb467187ede97ddf
davkk <dadulerk@gmail.com>
2024-02-21 18:58:41 +0100

    feat(nvim): increase on_yank highlight time

--- nvim/after/plugin/on_yank.lua
+++ nvim/after/plugin/on_yank.lua
@@ -3,6 +3,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Hightlight selection on yank',
     pattern = '*',
     callback = function()
-        vim.highlight.on_yank { timeout = 100 }
+        vim.highlight.on_yank { timeout = 150 }
     end,
 })
