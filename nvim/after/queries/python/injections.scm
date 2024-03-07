be7530929d1b3d0d29ac4e161f091b621bcc979b
davkk <dadulerk@gmail.com>
2024-03-07 20:57:49 +0100

    feat(nvim): add markdown comment injection into python

---
+++ nvim/after/queries/python/injections.scm
@@ -0,0 +1,8 @@
+; extends
+
+((
+  (comment) @_mdcomment
+  . (expression_statement 
+      (string (string_content) @injection.content)))
+  (#lua-match? @_mdcomment "^# %%%% %[markdown%]")
+  (#set! injection.language "markdown"))
