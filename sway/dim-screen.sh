25aee2b72278ab81497bba852b1b37f1338b7acd
davkk <dadulerk@gmail.com>
2024-03-07 20:58:33 +0100

    fix(sway): dim-screen was not working

--- sway/dim-screen.sh
+++ sway/dim-screen.sh
@@ -3,13 +3,8 @@
 min_brightness=0
 fade_step_time=0.01
 
-if [[ -z $fade_step_time ]]; then
-    light -S $1
-else
-    local level
-    initial_light=`light | awk -F'.' '{print $1}'`
-    for level in $(eval echo {$initial_light..$1}); do
-        light -S $1
-        sleep $fade_step_time
-    done
-fi
+initial_light=`light | awk -F'.' '{print $1}'`
+for level in $(eval echo {$initial_light..$min_brightness}); do
+    light -S $level
+    sleep $fade_step_time
+done
