d5abeae0025eb8f54780c6f77ce6343da3b67974
davkk <dadulerk@gmail.com>
2024-03-10 22:07:28 +0100

    fix: change luasnip keymaps to not collide with show signature

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -10,10 +10,17 @@ return {
         ls.filetype_extend("javascript", { "jsdoc" })
         ls.filetype_extend("typescript", { "javascript" })
 
-        vim.keymap.set({ "i" }, "<C-s>e", function() ls.expand() end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-k>", function()
+            if ls.jumpable(-1) then
+                ls.jump(-1)
+            end
+        end, { silent = true })
 
-        vim.keymap.set({ "i", "s" }, "<C-s>k", function() ls.jump(-1) end, { silent = true })
-        vim.keymap.set({ "i", "s" }, "<C-s>j", function() ls.jump(1) end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-j>", function()
+            if ls.expand_or_jumpable() then
+                ls.expand_or_jump()
+            end
+        end, { silent = true })
 
         vim.keymap.set({ "i", "s" }, "<C-e>", function()
             if ls.choice_active() then
