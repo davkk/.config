27c32470f3217d85b59aeade709380a832bae03f
davkk <dadulerk@gmail.com>
2024-05-16 18:26:49 +0200

    feat(nvim): open oil in current window

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -16,27 +16,6 @@ return {
             conceallevel = 3,
             concealcursor = "n",
         },
-        default_file_explorer = true,
-        restore_win_options = true,
-        skip_confirm_for_simple_edits = false,
-        prompt_save_on_select_new_entry = true,
-        keymaps = {
-            ["g?"] = "actions.show_help",
-            ["<C-y>"] = "actions.select",
-            ["<CR>"] = "actions.select",
-            ["<leader>v"] = "actions.select_vsplit",
-            ["<leader>x"] = "actions.select_split",
-            ["<leader>t"] = "actions.select_tab",
-            ["<leader>R"] = "actions.refresh",
-            ["<C-c>"] = "actions.close",
-            ["q"] = "actions.close",
-            ["-"] = "actions.parent",
-            ["_"] = "actions.open_cwd",
-            ["`"] = "actions.cd",
-            ["~"] = "actions.tcd",
-            ["g."] = "actions.toggle_hidden",
-        },
-        use_default_keymaps = false,
         view_options = { show_hidden = true, },
         float = {
             border = "solid",
@@ -47,7 +26,6 @@ return {
     config = function(_, opts)
         local oil = require("oil")
         oil.setup(opts)
-
-        vim.keymap.set("n", "<C-e>", oil.toggle_float, { noremap = true, silent = true })
+        vim.keymap.set("n", "<C-e>", oil.open, { noremap = true, silent = true })
     end,
 }
