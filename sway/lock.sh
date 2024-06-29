da393a49c8dd3169a1934587055ed02b3831ad55
davkk <dadulerk@gmail.com>
2024-06-29 15:39:45 +0200

    feat(sway): QoL updates, add autotiling script

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ -z $1 ]]; then
-    swaylock -f -c 111111 -s fill
-    exit 0
-fi
+# if [[ -z $1 ]]; then
+#     swaylock -f -c 111111 -s fill
+#     exit 0
+# fi
 
-wallpaper=$1
+# wallpaper=$1
 blurred=/tmp/blured_lock_wallpaper.jpg
 
-yes | ffmpeg -i $wallpaper -vf "gblur=sigma=44:steps=6" $blurred
+grim - | ffmpeg -y -i pipe: -vf "gblur=sigma=50:steps=6,eq=brightness=0.03" $blurred
 swaylock -f -i $blurred -s fill
+rm $blurred
