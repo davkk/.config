4e41e45bf261b366caea21fa50e79be7cbb542ef
davkk <dadulerk@gmail.com>
2024-08-22 23:23:10 +0200

    feat(nvim): update colorscheme

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -13,6 +13,7 @@ opt.number = true
 
 opt.laststatus = 3
 
+opt.cursorline = true
 opt.colorcolumn = "80"
 
 opt.scrolloff = 8
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
-local my_palette = {
+local diagnostics = {
     error = "#b4637a",
     warn = "#ea9d34",
     hint = "#907aa9",
@@ -17,7 +17,7 @@ return {
         extend_background_behind_borders = true,
 
         enable = {
-            terminal = true,
+            terminal = false,
             legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
             migrations = true,        -- Handle deprecated options automatically
         },
@@ -29,60 +29,52 @@ return {
         },
 
         groups = {
-            error = my_palette.error,
-            warn = my_palette.warn,
-            hint = my_palette.hint,
-            info = my_palette.info,
-
+            error = diagnostics.error,
+            warn = diagnostics.warn,
+            hint = diagnostics.hint,
+            info = diagnostics.info,
             todo = "love",
         },
 
         highlight_groups = {
-            -- line
-            CursorLine = { bg = "base" },
-            ColorColumn = { link = "CursorLine" },
+            ColorColumn = { bg = "base" },
+            CursorLine = { bg = "none" },
             CursorLineNr = { fg = "text", bold = true },
             LineNr = { fg = "highlight_high" },
             EndOfBuffer = { link = "LineNr" },
 
-            -- visual
-            Visual = { bg = "highlight_med" },
+            Visual = { bg = "overlay" },
+            Search = { fg = "none", bg = "highlight_med", blend = 75 },
+
             Comment = { link = "LineNr" },
 
-            -- menu
             Pmenu = { blend = 5 },
             PmenuSel = { bg = "rose", fg = "base" },
 
-            -- window
             WinSeparator = { bg = "none", fg = "overlay" },
+
             NormalFloat = { blend = 5 },
             FloatTitle = { bg = "base", fg = "highlight_med", blend = 5 },
             FloatBorder = { fg = "highlight_med", blend = 5 },
+
             ModeMsg = { link = "LineNr" },
 
-            -- statusline
             StatusLine = { fg = "subtle" },
             StatusLineTerm = { link = "StatusLine" },
             StatusLineTermNC = { link = "StatusLine" },
 
-            -- tabline
             TabLine = { fg = "subtle" },
             TabLineSel = { fg = "text", bg = "none" },
             TabLineFill = { bg = "none" },
 
-            -- diagnostics
-            SpellBad = { sp = my_palette.warn },
-            DiagnosticUnnecessary = { fg = "muted", sp = my_palette.hint, underline = true },
+            SpellBad = { sp = diagnostics.warn },
+            DiagnosticUnnecessary = { fg = "muted", sp = diagnostics.hint, underline = true },
 
-            -- lsp
-            ["@type.qualifier"] = { fg = "subtle" },
-            ["@function.method.call"] = { fg = "rose" },
-
-            -- quickfix list
             QuickFixLine = { bg = "text", blend = 15, bold = true },
 
-            -- search
-            Search = { fg = "none", bg = "highlight_med", blend = 75 },
+            ["@type.qualifier"] = { fg = "subtle" },
+
+            -- plugins
 
             -- lewis6991/gitsigns.nvim
             GitSignsAdd = { bg = "none" },
@@ -97,14 +89,6 @@ return {
             CmpItemMenu = { fg = "highlight_high" },
             CmpItemAbbrMatchFuzzy = { fg = "rose" },
             CmpItemKind = { fg = "iris" },
-
-            -- nvim-telescope/telescope.nvim
-            TelescopeNormal = { blend = 5 },
-            TelescopePromptNormal = { blend = 5 },
-            TelescopeBorder = { blend = 5 },
-            TelescopeTitle = { fg = "foam", bg = "foam", blend = 5, bold = false },
-            TelescopeSelection = { bg = "highlight_med" },
-            TelescopeSelectionCaret = { bg = "highlight_med", bold = true },
 
             -- nvim-treesitter/nvim-treesitter-context
             TreesitterContext = { bg = "none", link = "Normal" },
