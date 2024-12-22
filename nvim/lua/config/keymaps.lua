33177c63340e77e15940333c58eafdedf07f0e49
Dawid Karpiński <dawid.karp@int.pl>
2024-12-22 20:49:40 +0100

    feat(nvim): keymaps cleanup

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,10 +4,7 @@ local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>", opts)
 
-vim.keymap.set({ "n", "v" }, "<leader>p", '"_dP', opts)
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], opts)
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], opts)
-vim.keymap.set("n", "<leader>Y", [["+Y]], opts)
 
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
@@ -28,22 +25,16 @@ vim.keymap.set("n", "%", "%zz", opts)
 vim.keymap.set("n", "<C-o>", "<C-o>zz", opts)
 vim.keymap.set("n", "<C-i>", "<C-i>zz", opts)
 
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz", opts)
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz", opts)
+
 vim.keymap.set("n", "<A-Right>", "<C-w>5>", opts)
 vim.keymap.set("n", "<A-Left>", "<C-w>5<", opts)
 vim.keymap.set("n", "<A-Up>", "<C-w>2+", opts)
 vim.keymap.set("n", "<A-Down>", "<C-w>2-", opts)
 
-vim.keymap.set({ "n", "v" }, "k",
-    "v:count == 0 ? 'gk' : 'k'",
-    { expr = true, silent = true }
-)
-vim.keymap.set({ "n", "v" }, "j",
-    "v:count == 0 ? 'gj' : 'j'",
-    { expr = true, silent = true }
-)
-
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz", opts)
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz", opts)
+vim.keymap.set({ "n", "v" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set({ "n", "v" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 vim.keymap.set("n", "<left>", "gT", opts)
 vim.keymap.set("n", "<right>", "gt", opts)
