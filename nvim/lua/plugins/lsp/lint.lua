1780f7e132e2992dd37dd45df27b41e45fd14d0b
Dawid Karpiński <dawid.karp@int.pl>
2025-01-14 18:47:17 +0100

    feat(nvim): add custom `o2_linter` as linter

--- nvim/lua/plugins/lsp/lint.lua
+++ nvim/lua/plugins/lsp/lint.lua
@@ -3,7 +3,22 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     config = function()
         local lint = require("lint")
+        local parser = require("lint.parser")
         local util = require("lspconfig.util")
+
+        lint.linters.o2_linter = {
+            cmd = "python",
+            stdin = false,
+            args = { "/home/davkk/work/alice/O2Physics/Scripts/o2_linter.py" },
+            stream = "stdout",
+            ignore_exitcode = true,
+            parser = parser.from_pattern(
+                "(.+):(%d+):(.+)",
+                { "file", "lnum", "message" },
+                nil,
+                { source = "o2_linter", severity = vim.diagnostic.severity.INFO }
+            )
+        }
 
         lint.linters_by_ft = {
             python = { "ruff" },
@@ -12,12 +27,14 @@ return {
             typescript = { "eslint_d" },
             typescriptreact = { "eslint_d" },
             cmake = { "cmakelint" },
+            cpp = { "o2_linter" }
         }
 
         local root_patterns = {
             ruff = { "pyproject.toml", "ruff.toml", ".ruff.toml" },
             eslint_d = { ".eslintrc", ".eslintrc.js", ".eslintrc.json" },
             biomejs = { "biome.json" },
+            o2_linter = { "PWGCF", ".mega-linter.yml" },
         }
 
         local lint_augroup = vim.api.nvim_create_augroup("Lint", { clear = true })
