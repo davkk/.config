fa0a0e0c3bc9b6d928396a9f64d609382de16b06
Dawid Karpiński <dawid.karp@int.pl>
2025-01-16 13:36:46 +0100

    fix(sway): properly detect if there are screeshots for lock screen

--- sway/lock.sh
+++ sway/lock.sh
@@ -6,15 +6,13 @@ mkdir -p $tmpdir
 outputs=`swaymsg -t get_outputs | jq -r '.[] | select(.active == true) | .name'`
 args=""
 
-if [[ -d $tmpdir ]]; then
-    for output in $outputs; do
-        image=/tmp/swaylock/${output}.jpg
-        grim -o $output - \
-            | ffmpeg -y -i pipe: -vf "gblur=sigma=50:steps=6,eq=brightness=0.03" $image
+for output in $outputs; do
+    image=/tmp/swaylock/${output}.jpg
+    grim -o $output - | ffmpeg -y -i pipe: -vf "gblur=sigma=50:steps=6,eq=brightness=0.03" $image
+    args="$args -i $output:$image"
+done
 
-        args="$args -i $output:$image"
-    done
-
+if [ ! -z "`ls -A $tmpdir`" ]; then
     swaylock -f -s fill $args
 else
     swaylock -f -c 111111 -s fill
