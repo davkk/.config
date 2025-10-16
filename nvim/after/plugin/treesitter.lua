0a95bec6e4f49bb78ae2d9e883e7ab136bcebb89
Dawid Karpiński <dawid.karp@int.pl>
2025-10-16 20:11:37 +0200

    feat(nvim): migrate to nvim-treesitter main branch

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,34 +1,35 @@
-require("nvim-treesitter.configs").setup {
-    ensure_installed = {
-        "lua",
-        "luadoc",
-        "vim",
-        "vimdoc",
-        "bash",
-        "html",
-        "markdown",
-        "javascript",
-        "jsdoc",
-        "typescript",
-        "tsx",
-        "css",
-        "json",
-        "c",
-        "cpp",
-        "doxygen",
-        "python",
-        "angular",
-    },
-    sync_install = true,
-    auto_install = false,
-    highlight = { enable = true },
-    indent = {
-        enable = true,
-        disable = { "cpp" },
-    },
+require("nvim-treesitter").install {
+    "lua",
+    "luadoc",
+    "vim",
+    "vimdoc",
+    "bash",
+    "html",
+    "markdown",
+    "javascript",
+    "jsdoc",
+    "typescript",
+    "tsx",
+    "css",
+    "json",
+    "c",
+    "cpp",
+    "doxygen",
+    "python",
+    "angular",
 }
 
 vim.o.foldenable = true
 vim.o.foldlevel = 99
 vim.o.foldmethod = "expr"
-vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = ""
+
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+        local ok = pcall(vim.treesitter.start)
+        if ok then
+            vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+        end
+    end,
+})
