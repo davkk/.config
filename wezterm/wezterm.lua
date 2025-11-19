73b96fc3839761a6da8b100e26face59caa10d56
Dawid Karpiński <dawid.karp@int.pl>
2025-11-19 22:31:19 +0100

    ref(wezterm): better fancy tabs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,20 +5,33 @@ local action = wezterm.action
 -- startup
 config.default_prog = { "/usr/bin/env", "zsh", "-l" }
 
--- window
-config.window_decorations = "NONE"
-config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-config.window_close_confirmation = "AlwaysPrompt"
-config.skip_close_confirmation_for_processes_named = {}
-config.audible_bell = "Disabled"
-
 local gpus = wezterm.gui.enumerate_gpus()
 config.webgpu_preferred_adapter = gpus[1]
 config.front_end = "WebGpu"
 config.max_fps = 144
 
+-- window
+config.window_decorations = "NONE"
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.window_close_confirmation = "AlwaysPrompt"
+config.window_frame = {
+    font = wezterm.font { family = "Iosevka", weight = "Bold" },
+    font_size = 16,
+    active_titlebar_bg = "#000000",
+    inactive_titlebar_bg = "#000000",
+}
+
+config.skip_close_confirmation_for_processes_named = {}
+config.audible_bell = "Disabled"
+
 -- colors
 config.color_scheme = "neovim"
+config.colors = {
+    tab_bar = {
+        inactive_tab_edge = "#000000",
+    },
+}
+
 
 -- fonts
 config.font = wezterm.font_with_fallback { "Iosevka" }
@@ -27,7 +40,6 @@ config.warn_about_missing_glyphs = false
 config.underline_thickness = "0.08cell"
 
 -- tabs
-config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
@@ -37,11 +49,9 @@ wezterm.on("format-tab-title", function(tab)
     local tab_title = tab.tab_title
     local pane_title = tab.active_pane.title
     return {
+        { Background = { Color = "#000000" } },
         { Text = " " },
-        { Text = string.format("%d:", tab.tab_index + 1) },
-        { Text = "[" },
         { Text = (tab_title and #tab_title > 0) and tab_title or pane_title },
-        { Text = "]" },
         { Text = " " },
     }
 end)
