64452c3ea65e2112f95fb257bdc48c893ef2edb8
Dawid Karpiński <dawid.karp@int.pl>
2026-02-01 15:30:44 +0100

    feat(nvim): update quickfill config

--- nvim/after/plugin/quickfill.lua
+++ nvim/after/plugin/quickfill.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("i", "<C-q>", "<Plug>(quickfill-accept)")
+vim.keymap.set("i", "<C-S-q>", "<Plug>(quickfill-accept-replace)")
 vim.keymap.set("i", "<C-l>", "<Plug>(quickfill-accept-word)")
 vim.keymap.set("i", "<C-space>", "<Plug>(quickfill-trigger)")
 
@@ -6,7 +7,9 @@ vim.keymap.set("i", "<C-space>", "<Plug>(quickfill-trigger)")
 vim.g.quickfill = {
     n_prefix = 8,
     n_suffix = 8,
+    n_predict = 16,
     max_extra_chunks = 4,
     max_lsp_completion_items = 10,
     lsp_signature_help = true,
+    stop_on_trigger_char = false,
 }
